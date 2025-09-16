88f997999992e5f7040511048ed8caceb64550cd
Samaneh H. Moghaddam <78618962+hosseinisamaneh@users.noreply.github.com>
2025-09-16 16:23:04 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-// Smooth scroll for anchor links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        if(this.getAttribute('href').startsWith('#')){
+// Smooth scroll for sidebar links (if page has anchors)
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+    link.addEventListener('click', function(e){
+        const href = this.getAttribute('href');
+        if(href.startsWith("#")){
             e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            document.querySelector(href).scrollIntoView({behavior: 'smooth'});
         }
     });
 });
